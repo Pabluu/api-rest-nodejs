1259974baa1d06b6e693e16f9cac78a3449408dc
Pablo Henrique SC <pabloph0077@gmail.com>
2023-05-02 22:48:02 -0300

    fazendo consulta ao bd

--- nodejs/aulas/02-api-rest-nodejs/src/server.ts
+++ nodejs/aulas/02-api-rest-nodejs/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { knex } from './database'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello Man";
-});
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
 
-app.listen({ port: 3333 })
-.then(() => {
-  console.log('HTTP Server Running!')
+  return tables
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('http://localhost:3333/')
   app.close()
 })
